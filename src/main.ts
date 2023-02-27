9c4cc15c96557b3c1fb6e12f4cfa936c0578897f
Nekilc <nieaowei@gmail.com>
2023-02-28 02:11:51 +0800

    Add default grahpql

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,28 @@ import './style.css'
 import App from './App.vue'
 import {createRouter, createWebHistory} from "vue-router";
 import '@varlet/touch-emulator'
+import {DefaultApolloClient} from "@vue/apollo-composable";
+import {ApolloClient, createHttpLink, InMemoryCache} from "@apollo/client/core";
 
 const router = createRouter({
     history: createWebHistory(),
-    routes: [
+    routes: [],
+})
 
-    ],
+const httpLink = createHttpLink({
+    uri: 'https://api.hikit.io/auth/'
+})
+
+// Cache implementation
+const cache = new InMemoryCache()
+
+// Create the apollo client
+const apolloClient = new ApolloClient({
+    link: httpLink,
+    cache,
 })
 
 createApp(App)
     .use(router)
+    .provide(DefaultApolloClient, apolloClient)
     .mount('#app')
