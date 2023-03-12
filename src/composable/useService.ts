23943316e07a70805c8bd52a73b535e647d77245
Nekilc <nieaowei@gmail.com>
2023-03-12 15:08:11 +0800

    Add npm run g

--- src/composable/useService.ts
+++ src/composable/useService.ts
@@ -8,31 +8,62 @@ export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: 
 export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
 export type ReactiveFunction<TParam> = () => TParam;
 
-export const ProfileDocument = gql`
-    query profile {
-  profile
+export const GetProfileDocument = gql`
+    query getProfile {
+  profile {
+    id
+    name
+    email
+  }
 }
     `;
 
 /**
- * __useProfileQuery__
+ * __useGetProfileQuery__
  *
- * To run a query within a Vue component, call `useProfileQuery` and pass it any options that fit your needs.
- * When your component renders, `useProfileQuery` returns an object from Apollo Client that contains result, loading and error properties
+ * To run a query within a Vue component, call `useGetProfileQuery` and pass it any options that fit your needs.
+ * When your component renders, `useGetProfileQuery` returns an object from Apollo Client that contains result, loading and error properties
  * you can use to render your UI.
  *
  * @param options that will be passed into the query, supported options are listed on: https://v4.apollo.vuejs.org/guide-composable/query.html#options;
  *
  * @example
- * const { result, loading, error } = useProfileQuery();
+ * const { result, loading, error } = useGetProfileQuery();
  */
-export function useProfileQuery(options: VueApolloComposable.UseQueryOptions<ProfileQuery, ProfileQueryVariables> | VueCompositionApi.Ref<VueApolloComposable.UseQueryOptions<ProfileQuery, ProfileQueryVariables>> | ReactiveFunction<VueApolloComposable.UseQueryOptions<ProfileQuery, ProfileQueryVariables>> = {}) {
-  return VueApolloComposable.useQuery<ProfileQuery, ProfileQueryVariables>(ProfileDocument, {}, options);
+export function useGetProfileQuery(options: VueApolloComposable.UseQueryOptions<GetProfileQuery, GetProfileQueryVariables> | VueCompositionApi.Ref<VueApolloComposable.UseQueryOptions<GetProfileQuery, GetProfileQueryVariables>> | ReactiveFunction<VueApolloComposable.UseQueryOptions<GetProfileQuery, GetProfileQueryVariables>> = {}) {
+  return VueApolloComposable.useQuery<GetProfileQuery, GetProfileQueryVariables>(GetProfileDocument, {}, options);
 }
-export function useProfileLazyQuery(options: VueApolloComposable.UseQueryOptions<ProfileQuery, ProfileQueryVariables> | VueCompositionApi.Ref<VueApolloComposable.UseQueryOptions<ProfileQuery, ProfileQueryVariables>> | ReactiveFunction<VueApolloComposable.UseQueryOptions<ProfileQuery, ProfileQueryVariables>> = {}) {
-  return VueApolloComposable.useLazyQuery<ProfileQuery, ProfileQueryVariables>(ProfileDocument, {}, options);
+export function useGetProfileLazyQuery(options: VueApolloComposable.UseQueryOptions<GetProfileQuery, GetProfileQueryVariables> | VueCompositionApi.Ref<VueApolloComposable.UseQueryOptions<GetProfileQuery, GetProfileQueryVariables>> | ReactiveFunction<VueApolloComposable.UseQueryOptions<GetProfileQuery, GetProfileQueryVariables>> = {}) {
+  return VueApolloComposable.useLazyQuery<GetProfileQuery, GetProfileQueryVariables>(GetProfileDocument, {}, options);
 }
-export type ProfileQueryCompositionFunctionResult = VueApolloComposable.UseQueryReturn<ProfileQuery, ProfileQueryVariables>;
+export type GetProfileQueryCompositionFunctionResult = VueApolloComposable.UseQueryReturn<GetProfileQuery, GetProfileQueryVariables>;
+export const GetNameDocument = gql`
+    query getName {
+  profile {
+    name
+  }
+}
+    `;
+
+/**
+ * __useGetNameQuery__
+ *
+ * To run a query within a Vue component, call `useGetNameQuery` and pass it any options that fit your needs.
+ * When your component renders, `useGetNameQuery` returns an object from Apollo Client that contains result, loading and error properties
+ * you can use to render your UI.
+ *
+ * @param options that will be passed into the query, supported options are listed on: https://v4.apollo.vuejs.org/guide-composable/query.html#options;
+ *
+ * @example
+ * const { result, loading, error } = useGetNameQuery();
+ */
+export function useGetNameQuery(options: VueApolloComposable.UseQueryOptions<GetNameQuery, GetNameQueryVariables> | VueCompositionApi.Ref<VueApolloComposable.UseQueryOptions<GetNameQuery, GetNameQueryVariables>> | ReactiveFunction<VueApolloComposable.UseQueryOptions<GetNameQuery, GetNameQueryVariables>> = {}) {
+  return VueApolloComposable.useQuery<GetNameQuery, GetNameQueryVariables>(GetNameDocument, {}, options);
+}
+export function useGetNameLazyQuery(options: VueApolloComposable.UseQueryOptions<GetNameQuery, GetNameQueryVariables> | VueCompositionApi.Ref<VueApolloComposable.UseQueryOptions<GetNameQuery, GetNameQueryVariables>> | ReactiveFunction<VueApolloComposable.UseQueryOptions<GetNameQuery, GetNameQueryVariables>> = {}) {
+  return VueApolloComposable.useLazyQuery<GetNameQuery, GetNameQueryVariables>(GetNameDocument, {}, options);
+}
+export type GetNameQueryCompositionFunctionResult = VueApolloComposable.UseQueryReturn<GetNameQuery, GetNameQueryVariables>;
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
   ID: string;
@@ -42,17 +73,57 @@ export type Scalars = {
   Float: number;
 };
 
+export type EmailLoginParams = {
+  email: Scalars['String'];
+  password: Scalars['String'];
+};
+
+export type GithubLoginParams = {
+  code: Scalars['String'];
+};
+
+export type LoginParams = {
+  email?: InputMaybe<EmailLoginParams>;
+  github?: InputMaybe<GithubLoginParams>;
+};
+
+export type LoginResp = {
+  __typename?: 'LoginResp';
+  accessToken: Scalars['String'];
+  expires: Scalars['Int'];
+  idToken: Scalars['String'];
+  refreshToken: Scalars['String'];
+};
+
 export type Mutation = {
   __typename?: 'Mutation';
-  login: Scalars['String'];
+  delete: Scalars['String'];
+  login: LoginResp;
+};
+
+
+export type MutationLoginArgs = {
+  by: LoginParams;
+};
+
+export type Profile = {
+  __typename?: 'Profile';
+  email: Scalars['String'];
+  id: Scalars['String'];
+  name: Scalars['String'];
 };
 
 export type Query = {
   __typename?: 'Query';
-  profile: Scalars['String'];
+  profile: Profile;
 };
 
-export type ProfileQueryVariables = Exact<{ [key: string]: never; }>;
+export type GetProfileQueryVariables = Exact<{ [key: string]: never; }>;
 
 
-export type ProfileQuery = { __typename?: 'Query', profile: string };
+export type GetProfileQuery = { __typename?: 'Query', profile: { __typename?: 'Profile', id: string, name: string, email: string } };
+
+export type GetNameQueryVariables = Exact<{ [key: string]: never; }>;
+
+
+export type GetNameQuery = { __typename?: 'Query', profile: { __typename?: 'Profile', name: string } };
